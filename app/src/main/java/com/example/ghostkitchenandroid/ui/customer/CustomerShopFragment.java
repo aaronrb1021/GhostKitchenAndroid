405b717c85608f1f92c79d23e3087b71a607e77a
aaronrb1021 <bararb04@gmail.com>
2020-05-02 14:21:57 -0400

    finished api dialog for zip code

--- app/src/main/java/com/example/ghostkitchenandroid/ui/customer/CustomerShopFragment.java
+++ app/src/main/java/com/example/ghostkitchenandroid/ui/customer/CustomerShopFragment.java
@@ -3,6 +3,7 @@ package com.example.ghostkitchenandroid.ui.customer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,15 +11,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.ghostkitchenandroid.R;
 import com.example.ghostkitchenandroid.ui.lists.KitchenListAdapter;
 import com.example.ghostkitchenandroid.ui.lists.KitchenListFragment;
+import com.example.ghostkitchenandroid.utils.UserCredValidity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.resources.TextAppearance;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.w3c.dom.Text;
 
@@ -26,6 +40,7 @@ public class CustomerShopFragment extends Fragment {
 
     private CustomerShopViewModel customerShopViewModel;
     private TextView tvFilter;
+    private ImageView imageFilter;
     private View listContainer;
     private BottomNavigationView bottomNavigationView;
 
@@ -51,12 +66,15 @@ public class CustomerShopFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         tvFilter = view.findViewById(R.id.customer_shop_filter_tv);
+        imageFilter = view.findViewById(R.id.customer_shop_filter_image);
         listContainer = view.findViewById(R.id.customer_shop_list_container);
         bottomNavigationView = view.findViewById(R.id.customer_shop_bottom_nav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(makeNavListener());
 
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_shop_explore);
+
+        manageFilterButton();
     }
 
     @Override
@@ -79,4 +97,86 @@ public class CustomerShopFragment extends Fragment {
     private void displayKitchenList() {
         getParentFragmentManager().beginTransaction().replace(listContainer.getId(), KitchenListFragment.newInstance(KitchenListAdapter.MODE_CUSTOMER)).commit();
     }
+
+    private void manageFilterButton() {
+        View.OnClickListener onClickListener = (v) -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+            builder.setPositiveButton(android.R.string.cancel, ((dialog, which) -> {
+                dialog.dismiss();
+            }))
+                    .setTitle("Distance From Zip Code")
+                    .setView(makeLinearLayout())
+                    .create()
+                    .show();
+        };
+
+        tvFilter.setOnClickListener(onClickListener);
+        imageFilter.setOnClickListener(onClickListener);
+    }
+
+    private LinearLayout makeLinearLayout() {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextInputLayout textInputLayout = makeTextInput();
+
+        linearLayout.addView(textInputLayout);
+        linearLayout.addView(makeListView(textInputLayout));
+
+        return linearLayout;
+    }
+
+    private TextInputLayout makeTextInput() {
+        TextInputLayout textInputLayout = new TextInputLayout(getContext());
+        TextInputEditText textInputEditText = new TextInputEditText(getContext());
+
+        textInputEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        textInputEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        textInputLayout.setErrorEnabled(true);
+        textInputLayout.setPadding(450, 50, 450, 0);
+        textInputLayout.addView(textInputEditText);
+
+        textInputLayout.setHint("Zip Code");
+
+        return textInputLayout;
+    }
+
+    private ListView makeListView(TextInputLayout textInputLayout) {
+        String[] options = {"5 miles", "10 miles", "20 miles", "50 miles", "All"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_single_choice, options);
+
+        ListView listView = new ListView(getContext());
+
+        listView.setAdapter(adapter);
+
+        listView.setSelection(4);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            if (!UserCredValidity.isZipValid(textInputLayout.getEditText().getText().toString())) {
+                textInputLayout.setError(getString(R.string.required));
+            }
+        });
+
+
+        return listView;
+    }
+
 }
